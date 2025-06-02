00bb3f3d3dcb46b19e4a95ded828bcb0a7d1da43
Connor Evans <connor.l.evans.mil@army.mil>
2025-06-02 10:45:51 -0500

    setup; adds initial scaffolding

---
+++ src/main/java/com/example/modbpractice/ModBPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.modbpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModBPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModBPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/modbpractice/ModBPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modbpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModBPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
