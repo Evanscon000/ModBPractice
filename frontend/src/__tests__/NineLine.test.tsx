7d0324a58401f976627205556e9c015b47d89eea
Connor Evans <connor.l.evans.mil@army.mil>
2025-06-04 10:53:36 -0500

    feature; adds updated tests for input render, user behavior, and submit button to NineLine form

--- frontend/src/__tests__/NineLine.test.tsx
+++ frontend/src/__tests__/NineLine.test.tsx
@@ -3,6 +3,7 @@ import {render, screen} from "@testing-library/react";
 import {MemoryRouter} from "react-router-dom";
 import NineLine from "../components/NineLine.tsx";
 import '@testing-library/jest-dom';
+import {userEvent} from "@testing-library/user-event";
 
 
 describe('Nine Line Input Page', ()=> {
@@ -11,7 +12,7 @@ describe('Nine Line Input Page', ()=> {
          vi.clearAllMocks();
         });
 
-    it('should display the nine line app header with the input forms', () =>{
+    it('should display the nine line app header with the input forms', () => {
         render(
             <MemoryRouter>
                 <NineLine/>
@@ -20,25 +21,51 @@ describe('Nine Line Input Page', ()=> {
 
         expect(screen.getByRole('heading')).toBeVisible();
 
-        expect(screen.getByLabelText('Line 1')).toBeVisible()
-        expect(screen.getByLabelText('Line 2')).toBeVisible()
-        expect(screen.getByLabelText('Line 3')).toBeVisible()
-        expect(screen.getByLabelText('Line 4')).toBeVisible()
-        expect(screen.getByLabelText('Line 5')).toBeVisible()
-        expect(screen.getByLabelText('Line 6')).toBeVisible()
-        expect(screen.getByLabelText('Line 7')).toBeVisible()
-        expect(screen.getByLabelText('Line 8')).toBeVisible()
-        expect(screen.getByLabelText('Line 9')).toBeVisible()
+        // create a list (array)of input field names so the for...of loop is able to loop.
+        const lineNames = ["Line1","Line2","Line3","Line4","Line5","Line6","Line7","Line8","Line9",]
+
+        // the actual test portion of the loop
+        for (const label of lineNames) {
+            expect(screen.getByLabelText(label)).toBeVisible()
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+        // this loop above replaces the line below, the line below was repeated 9 times and excessive.
+        // the loop accomplishes DRY (don't repeat yourself)
+        // expect(screen.getByLabelText('Line1')).toBeVisible()
+        //--------------------------------------------------------------------------------------------------------------
+    });
+
+        it('lets user type in inputs and click submit button', async () =>{
+            render(
+                <MemoryRouter>
+                    <NineLine/>
+                </MemoryRouter>
+            )
+
+            //create a list (array) of input field names so the for...of can loop correctly.
+            const lineNames = ["Line1","Line2","Line3","Line4","Line5","Line6","Line7","Line8","Line9",]
+
+            //for...of loop (specific to JavaScript and TypeScript usable in React with JSX/TSX)
+            //for...of works with await and waits for it to finish before moving on unlike a forEach
+            for (const line of lineNames) {
+                const input = screen.getByPlaceholderText(new RegExp(line, "i"));
+                await userEvent.type(input, `Test ${line}`);
+                expect(input).toHaveValue(`Test ${line}`);
+            }
+
+            // ---------------------------------------------------------------------------------------------------------
+            // the for...of loop above replaces the code below (the code below was duplicated 9 times one for every line)
+            // using the for...of loop took 27 lines of code down to 6
+            // const Line1 = screen.getByPlaceholderText(/Line1/i);
+            // await userEvent.type(Line1, "Test Line1");
+            // expect(Line1).toHaveValue("Test Line1");
+            // ---------------------------------------------------------------------------------------------------------
+
+            const submitBtn = screen.getByRole('button', { name: /submit/i });
+            expect(submitBtn).toBeVisible();
+            await userEvent.click(submitBtn);
+        })
 
 
-        expect(screen.getByRole('button', {name: /submit/i })).toBeVisible()
-        expect(screen.getByRole('button', {name: /reset/i })).toBeVisible()
-
-
-
-
-    })
-
-
-
-})
+    });
--- frontend/src/components/NineLine.tsx
+++ frontend/src/components/NineLine.tsx
@@ -1,5 +1,32 @@
+import {useEffect, useState} from "react";
+// import {useNavigate} from "react-router-dom";
 
 const NineLine = () => {
+    // const  navigate = useNavigate()
+
+    const [formData, setFormData] = useState({
+        Line1: '',
+        Line2: '',
+        Line3: '',
+        Line4: '',
+        Line5: '',
+        Line6: '',
+        Line7: '',
+        Line8: '',
+        Line9: ''
+    });
+
+    // use this function for developing on the dev tool (f12)
+    // useEffect(() => {
+    //     // console.log("Updated formData:", formData);
+    // }, [formData]);
+
+    // this handleChange is the function that handles the logic behind updating individual fields of text
+    const handleChange = (e:any)=>{
+        const {name, value} = e.target;
+        setFormData( prevState => ({...prevState,[name]: value}))
+    }
+
     return (
         <div>
             <h1> Welcome to the Nine Line Reporting App </h1>
@@ -7,77 +34,96 @@ const NineLine = () => {
             <div>
             <input
                 type = {'text'}
-                placeholder={'Line 1'}
-                name={'Line 1'}
-                aria-label={'Line 1'}/>
+                placeholder={'Line1'}
+                name={'Line1'}
+                aria-label={'Line1'}
+                value={formData.Line1}
+                onChange={handleChange}/>
             </div>
 
             <div>
             <input
                 type={'text'}
-                placeholder={'Line 2'}
-                name={'Line 2'}
-                aria-label={'Line 2'}/>
+                placeholder={'Line2'}
+                name={'Line2'}
+                aria-label={'Line2'}
+                value={formData.Line2}
+                onChange={handleChange}/>
             </div>
 
             <div>
                 <input
                 type={'text'}
-                placeholder={'Line 3'}
-                name={'Line 3'}
-                aria-label={'Line 3'}/>
+                placeholder={'Line3'}
+                name={'Line3'}
+                aria-label={'Line3'}
+                value={formData.Line3}
+                onChange={handleChange}/>
             </div>
 
             <div>
                 <input
                     type={'text'}
-                    placeholder={'Line 4'}
-                    name={'Line 4'}
-                    aria-label={'Line 4'}/>
+                    placeholder={'Line4'}
+                    name={'Line4'}
+                    aria-label={'Line4'}
+                    value={formData.Line4}
+                    onChange={handleChange}/>
             </div>
 
             <div>
                 <input
                     type={'text'}
-                    placeholder={'Line 5'}
-                    name={'Line 5'}
-                    aria-label={'Line 5'}/>
+                    placeholder={'Line5'}
+                    name={'Line5'}
+                    aria-label={'Line5'}
+                    value={formData.Line5}
+                    onChange={handleChange}/>
             </div>
 
             <div>
                 <input
                     type={'text'}
-                    placeholder={'Line 6'}
-                    name={'Line 6'}
-                    aria-label={'Line 6'}/>
+                    placeholder={'Line6'}
+                    name={'Line6'}
+                    aria-label={'Line6'}
+                    value={formData.Line6}
+                    onChange={handleChange}/>
             </div>
 
             <div>
                 <input
                 type={'text'}
-                placeholder={'Line 7'}
-                name={'Line 7'}
-                aria-label={'Line 7'}/>
+                placeholder={'Line7'}
+                name={'Line7'}
+                aria-label={'Line7'}
+                value={formData.Line7}
+                onChange={handleChange}/>
             </div>
 
             <div>
                 <input
                     type={'text'}
-                    placeholder={'Line 8'}
-                    name={'Line 8'}
-                    aria-label={'Line 8'}/>
+                    placeholder={'Line8'}
+                    name={'Line8'}
+                    aria-label={'Line8'}
+                    value={formData.Line8}
+                    onChange={handleChange}/>
             </div>
 
             <div>
                 <input
                     type={'text'}
-                    placeholder={'Line 9'}
-                    name={'Line 9'}
-                    aria-label={'Line 9'}/>
+                    placeholder={'Line9'}
+                    name={'Line9'}
+                    aria-label={'Line9'}
+                    value={formData.Line9}
+                    onChange={handleChange}/>
             </div>
 
             <button type={'submit'}>Submit</button>
-            <button type={'reset'}>Reset</button>
+            {/*<button type={'button'} onClick={()=>navigate('/')}>Exit</button>*/}
+
         </div>
 
     )
